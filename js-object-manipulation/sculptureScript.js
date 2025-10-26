d78431aaa4a3968054a3c7911e8c70f655b4174e
Emily <sha@sfsu.edu>
2025-10-26 03:26:08 -0700

    Enhance sculpture display and compute lengths
    Refactor sculpture display and add lengths calculation

--- js-object-manipulation/sculptureScript.js
+++ js-object-manipulation/sculptureScript.js
@@ -1,10 +1,24 @@
 const sculptureList = require('./data.js'); // import data.js
 
-const element = sculptureList[1] // <---- comment out this line in your solution!
-
-
-// this following snippet is just to show how to iterate an object's keys
-// comment this out in your solution!
-for (const key in element){
-    console.log(`${key}: ${element[key].length}`)
+let index = 0;
+function showSculpture() {
+  const sculpture = sculptureList[index];
+  console.log(`Name: ${sculpture.name}`);
+  console.log(`Artist: ${sculpture.artist}`);
+  console.log(`Description: ${sculpture.description}`);
+  console.log(`Image URL: ${sculpture.url}`);
 }
+showSculpture();
+
+
+let sculptureListLengths = [];
+
+sculptureList.forEach((sculpture) => {
+  const lengths = {};
+  for (let key in sculpture) {
+    lengths[key] = sculpture[key].length;
+  }
+  sculptureListLengths.push(lengths);
+});
+
+console.log(sculptureListLengths[0]);
